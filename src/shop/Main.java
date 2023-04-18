1668772cfe59fa5a65ada996be57b7897dfc1716
MaksymRudy <mail@google.com>
2023-04-18 14:41:24 +0300

    add department sportGood

--- src/shop/Main.java
+++ src/shop/Main.java
@@ -17,6 +17,7 @@ public class Main {
         Good semolina = new Good("Semolina");
         groceryGoods.add(semolina);
         groceryGoods.add(new Good("Rice"));
+        groceryGoods.add(new Good("Buckwheat"));
         shopDepartmentToGoods.put(departmentGrocery, groceryGoods);
 
         ArrayList<Good> groceriesGoods = shopDepartmentToGoods.get(departmentGrocery);
@@ -24,22 +25,33 @@ public class Main {
 
 
 
-        ShopDepartment departmentAuto = new ShopDepartment("Auto Good");
+        ShopDepartment departmentAuto = new ShopDepartment("Department Auto");
 
         ArrayList<Good> autoGoods = new ArrayList<>();
 
         Good tire = new Good("Tire");
         autoGoods.add(tire);
         autoGoods.add(new Good("Oil"));
+        autoGoods.add(new Good("Antifreeze"));
         shopDepartmentToGoods.put(departmentAuto, autoGoods);
 
         ArrayList<Good> autosGoods = shopDepartmentToGoods.get(departmentAuto);
         out.println(autosGoods);
 
 
+        ShopDepartment departmentSport = new ShopDepartment("Department Sport");
+
+        ArrayList<Good> sportGoods = new ArrayList<>();
+
+        Good sneakers = new Good("Sneakers");
+        sportGoods.add(sneakers);
+        sportGoods.add(new Good("T-shirt"));
+        sportGoods.add(new Good("Trousers"));
+        shopDepartmentToGoods.put(departmentSport, sportGoods);
+        out.println(sportGoods);
+
         tire.setTitle("tire2");
         out.println(shopDepartmentToGoods);
-
 
 
 
