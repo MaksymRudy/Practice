423b2be646fd16a133b8f5d15f1966c3b0134d78
MaksymRudy <mail@google.com>
2023-04-30 18:11:07 +0300

    add in package ablazzing new package lesson2Peremennie

---
+++ src/ablazzing/lesson2Peremennie/Variables.java
@@ -0,0 +1,11 @@
+package ablazzing.lesson2Peremennie;
+
+public class Variables {
+    public static void main(String[] args) {
+        // Переменная это именнованная ссылка на какие-то данные (которые мы сами задаём)
+        int countApples = 3;  // инициализация - присвоение значения
+        // примитивный тип данных - группа типов данных которые приобразуются в двоичный код
+        // и переменная хранит значение этого кода
+        System.out.println(countApples);
+    }
+}
